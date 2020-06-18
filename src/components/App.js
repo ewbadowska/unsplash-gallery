7f5590b672ef9066a6ec2de19f618d1c1da50242
Ewelina Badowska <ewbadowska@gmail.com>
2020-06-19 00:20:20 +0200

    use axios to make API call

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 import '../App.css';
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
+			}
+		});
 	}
 
 	render() {
