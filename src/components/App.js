906fcce5cad12c62e9ac3f09664e9b8f6ccd68ed
Ewelina Badowska <ewbadowska@gmail.com>
2020-06-18 22:31:31 +0200

    add search bar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import '../App.css';
+
+function App() {
+    return (
+      <div>
+      <SearchBar />
+  </div>
+    );
+}
+
+export default App;
+
